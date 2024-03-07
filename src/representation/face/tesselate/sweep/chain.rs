5c1b7a6897102c82be8a20c0afdf76a96fbc4dcb
Eric Skaliks <eric@skaliks.blue>
2024-03-07 06:29:56 +0100

    simplify

--- src/representation/face/tesselate/sweep/chain.rs
+++ src/representation/face/tesselate/sweep/chain.rs
@@ -40,10 +40,6 @@ impl SweepReflexChain {
         self.d
     }
 
-    pub fn last(&self) -> usize {
-        self.stack.last().unwrap().clone()
-    }
-
     pub fn first(&self) -> usize {
         self.stack.first().unwrap().clone()
     }
@@ -56,79 +52,98 @@ impl SweepReflexChain {
     }
 
     /// Add a new value to the left reflex chain
-    pub fn left<V: IndexType, P: Payload>(
+    pub fn add<V: IndexType, P: Payload>(
         &mut self,
         value: usize,
         indices: &mut Vec<V>,
         vec2s: &Vec<IndexedVertexPoint<P::Vec2, P::S>>,
+        d: SweepReflexChainDirection,
     ) -> &Self {
         #[cfg(feature = "sweep_debug_print")]
         println!("left: {:?} {} {:?}", self.d, value, self.stack);
-        match self.d {
-            SweepReflexChainDirection::None => {
-                assert!(self.stack.len() <= 1);
-                self.stack.push(value);
-                self.d = SweepReflexChainDirection::Left;
-            }
-            SweepReflexChainDirection::Left => {
-                assert!(self.stack.len() >= 1);
+        if self.d == SweepReflexChainDirection::None {
+            assert!(self.stack.len() <= 1);
+            self.stack.push(value);
+            self.d = d;
+        } else if self.d == d {
+            assert!(self.stack.len() >= 1);
 
-                // draw triangles while they are visible
-                loop {
-                    let l = self.stack.len();
-                    if l <= 1 {
-                        break;
-                    }
-                    let angle = vec2s[value]
-                        .vec
-                        .angle(vec2s[self.stack[l - 1]].vec, vec2s[self.stack[l - 2]].vec);
+            // draw triangles while they are visible
+            loop {
+                let l = self.stack.len();
+                if l <= 1 {
+                    break;
+                }
+                let angle = vec2s[value]
+                    .vec
+                    .angle(vec2s[self.stack[l - 1]].vec, vec2s[self.stack[l - 2]].vec);
+                if d == SweepReflexChainDirection::Left {
                     if angle > P::S::ZERO {
                         break;
                     }
-                    #[cfg(feature = "sweep_debug_print")]
-                    println!(
-                        "create vis l: {:?}",
-                        [self.stack[l - 1], self.stack[l - 2], value]
-                    );
                     indices.extend([
                         V::new(self.stack[l - 1]),
                         V::new(value),
                         V::new(self.stack[l - 2]),
                     ]);
-                    self.stack.pop();
-                }
-
-                // remember on more for the same direction
-                self.stack.push(value);
-            }
-            SweepReflexChainDirection::Right => {
-                assert!(self.stack.len() >= 1);
-                // place the next triangle!
-                if self.stack.len() == 1 {
-                    self.stack.push(value);
-                    self.d = SweepReflexChainDirection::Left;
                 } else {
-                    // there is enough on the stack to consume
-                    for i in 1..self.stack.len() {
+                    if angle < P::S::ZERO {
+                        break;
+                    }
+                    indices.extend([
+                        V::new(self.stack[l - 1]),
+                        V::new(self.stack[l - 2]),
+                        V::new(value),
+                    ]);
+                }
+                #[cfg(feature = "sweep_debug_print")]
+                println!(
+                    "create vis: {:?}",
+                    [self.stack[l - 1], self.stack[l - 2], value]
+                );
+
+                self.stack.pop();
+            }
+
+            // remember on more for the same direction
+            self.stack.push(value);
+        } else {
+            assert!(self.stack.len() >= 1);
+            // place the next triangle!
+            if self.stack.len() == 1 {
+                self.stack.push(value);
+                self.d = d;
+            } else {
+                // there is enough on the stack to consume
+                for i in 1..self.stack.len() {
+                    if d == SweepReflexChainDirection::Left {
                         indices.extend([
                             V::new(self.stack[i - 1]),
                             V::new(value),
                             V::new(self.stack[i]),
                         ]);
-                        #[cfg(feature = "sweep_debug_print")]
-                        println!(
-                            "create mul l: {:?}",
-                            [self.stack[i - 1], self.stack[i], value]
-                        );
+                    } else {
+                        indices.extend([
+                            V::new(self.stack[i - 1]),
+                            V::new(self.stack[i]),
+                            V::new(value),
+                        ]);
                     }
-                    let last = self.stack.pop().unwrap();
-                    self.stack.clear();
-                    self.stack.push(last);
-                    self.stack.push(value);
-                    self.d = SweepReflexChainDirection::Left;
+
+                    #[cfg(feature = "sweep_debug_print")]
+                    println!(
+                        "create mul l: {:?}",
+                        [self.stack[i - 1], self.stack[i], value]
+                    );
                 }
+                let last = self.stack.pop().unwrap();
+                self.stack.clear();
+                self.stack.push(last);
+                self.stack.push(value);
+                self.d = d;
             }
         }
+
         self
     }
 
@@ -139,80 +154,20 @@ impl SweepReflexChain {
         indices: &mut Vec<V>,
         vec2s: &Vec<IndexedVertexPoint<P::Vec2, P::S>>,
     ) -> &Self {
-        #[cfg(feature = "sweep_debug_print")]
-        println!("right: {:?} {} {:?}", self.d, value, self.stack);
-        match self.d {
-            SweepReflexChainDirection::None => {
-                assert!(self.stack.len() <= 1);
-                self.stack.push(value);
-                self.d = SweepReflexChainDirection::Right;
-            }
-            SweepReflexChainDirection::Right => {
-                assert!(self.stack.len() >= 1);
+        self.add::<V, P>(value, indices, vec2s, SweepReflexChainDirection::Right)
+    }
 
-                // draw triangles while they are visible
-                loop {
-                    let l = self.stack.len();
-                    if l <= 1 {
-                        break;
-                    }
-                    let angle = vec2s[value]
-                        .vec
-                        .angle(vec2s[self.stack[l - 1]].vec, vec2s[self.stack[l - 2]].vec);
-                    if angle < P::S::ZERO {
-                        break;
-                    }
-                    #[cfg(feature = "sweep_debug_print")]
-                    println!(
-                        "create vis r: {:?}",
-                        [self.stack[l - 1], self.stack[l - 2], value]
-                    );
-                    indices.extend([
-                        V::new(self.stack[l - 1]),
-                        V::new(self.stack[l - 2]),
-                        V::new(value),
-                    ]);
-                    self.stack.pop();
-                }
-
-                // remember on more for the same direction
-                self.stack.push(value);
-            }
-            SweepReflexChainDirection::Left => {
-                assert!(self.stack.len() >= 1);
-                // place the next triangle!
-                if self.stack.len() == 1 {
-                    self.stack.push(value);
-                    self.d = SweepReflexChainDirection::Right;
-                } else {
-                    // there is enough on the stack to consume
-                    for i in 1..self.stack.len() {
-                        // TODO: assert that they are visible
-                        indices.extend([
-                            V::new(self.stack[i - 1]),
-                            V::new(self.stack[i]),
-                            V::new(value),
-                        ]);
-                        #[cfg(feature = "sweep_debug_print")]
-                        println!(
-                            "create mul r: {:?}",
-                            [self.stack[i - 1], self.stack[i], value]
-                        );
-                    }
-                    let last = self.stack.pop().unwrap();
-                    self.stack.clear();
-                    self.stack.push(last);
-                    self.stack.push(value);
-                    self.d = SweepReflexChainDirection::Right;
-                }
-            }
-        }
-        self
+    /// Add a new value to the left reflex chain
+    pub fn left<V: IndexType, P: Payload>(
+        &mut self,
+        value: usize,
+        indices: &mut Vec<V>,
+        vec2s: &Vec<IndexedVertexPoint<P::Vec2, P::S>>,
+    ) -> &Self {
+        self.add::<V, P>(value, indices, vec2s, SweepReflexChainDirection::Left)
     }
 
     pub fn is_done(&self) -> bool {
         self.stack.len() <= 2
-        //&& self.d != SweepReflexChainDirection::Left
-        // && self.d != SweepReflexChainDirection::Right
     }
 }
