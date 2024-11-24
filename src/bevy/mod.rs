bc4fd58d072db3004e269a89b7c9a65682e6abb3
Eric Skaliks <26843285+Agapanthus@users.noreply.github.com>
2024-11-24 14:07:31 +0100

    fix ci

--- src/bevy/mod.rs
+++ src/bevy/mod.rs
@@ -1,15 +1,19 @@
 //! This module contains the bevy-specific implementations
 
-mod gizmo;
 mod math;
 mod mesh2d;
 mod mesh3d;
 mod vertex_payload_2d;
 mod vertex_payload_3d;
 
-pub use gizmo::*;
 pub use math::*;
 pub use mesh2d::*;
 pub use mesh3d::*;
 pub use vertex_payload_2d::*;
 pub use vertex_payload_3d::*;
+
+#[cfg(feature = "gizmo")]
+mod gizmo;
+
+#[cfg(feature = "gizmo")]
+pub use gizmo::*;
